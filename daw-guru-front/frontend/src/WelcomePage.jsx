775aec4b4563f40b8421ef6568a506e0f6fa4443
craig needham <creativesaints247@gmail.com>
2026-02-12 20:19:15 +0000

    feat(frontend): set welcome screen background to public image

--- daw-guru-front/frontend/src/WelcomePage.jsx
+++ daw-guru-front/frontend/src/WelcomePage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 function WelcomePage() {
   return (
-    <div style={{width:'100vw',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',background:'#232323'}}>
-      <img src={require('../../background.png')} alt="Welcome" style={{maxWidth:'100%',maxHeight:'100%',objectFit:'contain',borderRadius:24,boxShadow:'0 4px 32px #0008'}} />
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: "url('/background.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      {/* Optionally, add overlay or content here */}
     </div>
   );
 }
